d8dbd9e38eb60f3029ffc4b73c8f23bc84642636
nick3499 <indiessance@gmail.com>
2016-04-25 14:22:01 -0500

    Added files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = require('app');
+var BrowserWindow = require('browser-window');
+var win = null;
+
+app.on('ready', function() {
+  win = new BrowserWindow({
+    width: 1300,
+    height: 800
+  });
+  win.loadUrl('file://' + __dirname + '/blue-disk-array.html')
+});
